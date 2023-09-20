420ae90b0b2fd80a20a898f1a7595e100d952c4a
csm031231 <csumim@naver.com>
2023-09-20 19:58:43 +0900

    20230920

---
+++ C세미나/week01/Helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5;
+	int b = 3;
+
+	printf("%d %d\n", a, b);
+
+	return 0;
+}
---
+++ C세미나/week01/자료형.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5;
+	double b = 3.3;
+	float c = 5.5;
+	char d = 'a';
+
+	printf("%d %lf %f %c\n", a, b, c, d);
+
+	return 0;
+}
