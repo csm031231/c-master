7b54422553c9f59e706158bda7185d6f2ae89d6d
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-10-15 00:08:24 +0900

    2023.10.14

---
+++ C세미나/C세미나 과제/2단~9단.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 2; i <= 9; i++) {
+        printf("%d´Ü\n", i);
+        for (j = 1; j <= 9; j++) {
+            printf("%d * %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
