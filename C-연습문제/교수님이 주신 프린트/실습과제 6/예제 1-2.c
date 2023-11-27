c70fff10aec009465695cb38f95c47d79054f03e
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-11-27 21:59:38 +0900

    2023.11.27

--- C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-2.c
+++ C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-2.c
@@ -1,6 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int sumOfRange(int n1, int n2);
+
+int main()
+{
+	int n1, n2, nsum;
+	scanf_s("%d %d", &n1, &n2);
+	nsum = sumOfRange(n1, n2);
+	printf("%dë¶€í„° %dê¹Œì§€ì˜ í•©ì€ %d\n", n1, n2, nsum);
+}
 int sumOfRange(int n1, int n2) 
 {
 	
@@ -10,12 +19,4 @@ int sumOfRange(int n1, int n2)
 		sum = sum + i;
 	}
 	return sum;
-}
-
-int main()
-{
-	int n1, n2, nsum;
-	scanf_s("%d %d", &n1, &n2);
-	nsum = sumOfRange(n1, n2);
-	printf("%dºÎÅÍ %d±îÁöÀÇ ÇÕÀº %d\n", n1, n2, nsum);
 }
