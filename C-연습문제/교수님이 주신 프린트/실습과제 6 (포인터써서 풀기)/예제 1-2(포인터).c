2997a93eabf604acbfa8b1f5afcfd8927e75d1b4
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-11-27 21:57:53 +0900

    2023.11.27

--- C-연습문제/교수님이 주신 프린트/실습과제 6 (포인터써서 풀기)/예제 1-2(포인터).c
+++ C-연습문제/교수님이 주신 프린트/실습과제 6 (포인터써서 풀기)/예제 1-2(포인터).c
@@ -7,7 +7,7 @@ int main()
 	int n1, n2, nsum;
 	scanf_s("%d %d", &n1, &n2);
 	nsum = sumOfRange(&n1, &n2);
-	printf("%dºÎÅÍ %d±îÁöÀÇ ÇÕÀº %d\n", n1, n2, nsum);
+	printf("%dë¶€í„° %dê¹Œì§€ì˜ í•©ì€ %d\n", n1, n2, nsum);
 }
 
 int sumOfRange(int *n1, int *n2)
