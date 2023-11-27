206867d0d4b24c5906e0af2ad33442e88099d61d
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-11-27 21:41:51 +0900

    2023.11.27

---
+++ C-연습문제/교수님이 주신 프린트/실습과제 6 (포인터써서 풀기)/예제 1-2(포인터).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sumOfRange(int *n1, int *n2);
+
+int main()
+{
+	int n1, n2, nsum;
+	scanf_s("%d %d", &n1, &n2);
+	nsum = sumOfRange(&n1, &n2);
+	printf("%d부터 %d까지의 합은 %d\n", n1, n2, nsum);
+}
+
+int sumOfRange(int *n1, int *n2)
+{
+
+	int sum = 0;
+
+	for (int i = *n1; i <= *n2; i++) {
+		sum = sum + i;
+	}
+	return sum;
+}
---
+++ C-연습문제/교수님이 주신 프린트/실습과제 6 (포인터써서 풀기)/예제 3-1(포인터).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int find_max(int *idata, int *size);
+
+int main() {
+	int idata[5] = { 1, 5, 2, 8, 4 };
+
+	int n = find_max(&idata, sizeof(idata));
+	printf("%d", n);
+}
+
+int find_max(int *idata, int *size) {
+	int max = idata[5];
+
+	for (int i = 0; i <= size; i++)
+		if (idata[i] > max) {
+			max = idata[i];
+		}
+		
+
+	return max;
+}
