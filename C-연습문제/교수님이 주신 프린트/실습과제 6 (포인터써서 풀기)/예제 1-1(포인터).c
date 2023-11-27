2c623b439e285c983121acbb45b60063b71b1494
csm031231 <scumim@naver.com>
2023-11-27 21:39:22 +0900

    C-연습문제

---
+++ C-연습문제/교수님이 주신 프린트/실습과제 6 (포인터써서 풀기)/예제 1-1(포인터).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int compute_sum_1toN(int *p); //int compute_sum_1toN(int p);
+
+int main() 
+{
+	int n, sum;
+	scanf_s("%d", &n);
+
+	sum = compute_sum_1toN(&n); //int compute_sum_1toN(n);
+
+	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+
+}
+int compute_sum_1toN(int *p) //int compute_sum_1toN(int p);
+{
+	int i, sum = 0;
+
+	for (i = 1; i <= *p; i++) //for (i = 1; i <= p; i++)
+		sum = sum + i;
+
+	return sum;
+}
