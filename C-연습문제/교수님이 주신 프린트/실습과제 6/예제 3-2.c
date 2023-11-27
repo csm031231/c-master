c83455cdd2782387414c4b0aa7450ce5d240b8d9
csm031231 <scumim@naver.com>
2023-11-27 21:31:02 +0900

    C-연습문제

---
+++ C-연습문제/교수님이 주신 프린트/실습과제 6/예제 3-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+double average(int idata[], int size);
+
+int main() {
+	int idata[5] = { 1, 5, 2, 8, 4 };
+
+	double d = average(idata, sizeof(idata)/sizeof(int));
+
+	printf("%.1lf", d);
+}
+
+double average(int idata[], int size) {
+	double avg = 0;
+	int sum = 0;
+
+	for (int i = 0; i < size; i++) {
+		sum += idata[i];
+	}
+	avg = (double)sum / size;
+	
+	return avg;
+}
