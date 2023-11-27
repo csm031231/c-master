b7ede86afb6c3f154f4f3423014e8acadcacd1bc
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-11-27 22:00:26 +0900

    2023.11.27

--- C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-4.c
+++ C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-4.c
@@ -8,8 +8,8 @@ int main() {
 	scanf_s("%d %d", &n1, &n2);
 	a = mult(n1, n2);
 	double b = div(n1, n2);
-	printf("%d¿Í %dÀÇ °öÀº %d\n", n1, n2, a);
-	printf("%d¿Í %dÀÇ ºñÀ²Àº %lf\n", n1, n2, b);
+	printf("%dì™€ %dì˜ ê³±ì€ %d\n", n1, n2, a);
+	printf("%dì™€ %d ë¹„ìœ¨ì€ %lf\n", n1, n2, b);
 }
 int mult(int n1, int n2) 
 {
