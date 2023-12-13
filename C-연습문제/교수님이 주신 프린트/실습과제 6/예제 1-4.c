c46ac3190f558190b30beed1e07304afbb745428
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-12-14 01:54:15 +0900

    Update 예제 1-4.c

--- C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-4.c
+++ C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-4.c
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-int mult(int n1, int n2);
-double div(int n1, int n2);
-
-int main() {
-	int n1, n2, a;
-	
-	scanf_s("%d %d", &n1, &n2);
-	a = mult(n1, n2);
-	double b = div(n1, n2);
-	printf("%d¿Í %dÀÇ °öÀº %d\n", n1, n2, a);
-	printf("%d¿Í %dÀÇ ºñÀ²Àº %lf\n", n1, n2, b);
-}
-int mult(int n1, int n2) 
-{
-	int a = n1 * n2;
-}
-double div(int n1, int n2)
-{
-	double b = n1 / n2;
-}
-=======
 #include <stdio.h>
 int mult(int n1, int n2);
 double div(int n1, int n2);
@@ -32,7 +9,7 @@ int main() {
 	a = mult(n1, n2);
 	double b = div(n1, n2);
 	printf("%dì™€ %dì˜ ê³±ì€ %d\n", n1, n2, a);
-	printf("%dì™€ %d ë¹„ìœ¨ì€ %lf\n", n1, n2, b);
+        printf("%dì™€ %dì˜ ë¹„ìœ¨ì€ %lf\n", n1, n2, b);
 }
 int mult(int n1, int n2) 
 {
@@ -42,4 +19,3 @@ double div(int n1, int n2)
 {
 	double b = n1 / n2;
 }
->>>>>>> b7ede86afb6c3f154f4f3423014e8acadcacd1bc
