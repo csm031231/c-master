813e01d90818cca5712a453a8b7494db1d6a20d7
csm031231 <144320859+csm031231@users.noreply.github.com>
2023-12-14 01:52:51 +0900

    Update 예제 1-1.c

--- C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-1.c
+++ C-연습문제/교수님이 주신 프린트/실습과제 6/예제 1-1.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int compute_sum_1toN(int number);
 
@@ -8,7 +7,7 @@ int main()
 	scanf_s("%d", &n);
 
 	nsum = compute_sum_1toN(n);
-	printf("1ºÎÅÍ %d±îÁö ÇÕÀº : %d", n, nsum);
+	printf("1ë¶€í„° %dê¹Œì§€ í•©ì€ : %d", n, nsum);
 
 	return 0;
 }
@@ -21,27 +20,3 @@ int compute_sum_1toN(int number) {
 	}
 		return sum;
 }
-=======
-#include <stdio.h>
-int compute_sum_1toN(int number);
-
-int main() 
-{
-	int n, nsum;
-	scanf_s("%d", &n);
-
-	nsum = compute_sum_1toN(n);
-	printf("1ë¶€í„° %dê¹Œì§€ì˜ í•©: %d", n, nsum);
-
-	return 0;
-}
-
-int compute_sum_1toN(int number) {
-	int sum = 0;
-
-	for (int i = 1; i <= number; i++) {
-		sum = sum + i;
-	}
-		return sum;
-}
->>>>>>> b7ede86afb6c3f154f4f3423014e8acadcacd1bc
